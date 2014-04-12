c7d0c74d060c7ba502213a145777ef78176f32f7
sjas <teilzeittresor@web.de>
2014-04-12 21:54:02 +0200

    12.rb

---
+++ 12.rb
@@ -0,0 +1,18 @@
+require 'open-uri'
+
+open("https://www.ruby-lang.org/en") do |f|
+#   f.each_line {|line| p line}
+    puts f.base_uri
+    puts f.content_type
+    puts f.charset
+    puts f.content_encoding
+    puts f.last_modified
+end
+
+# require = file level include
+# include = load methods on language level
+
+# ruby looks for libraries here, use in irb
+# $ puts $:
+# or 
+# $ puts $LOAD_PATH
