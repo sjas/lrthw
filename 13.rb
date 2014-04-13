bc4984e8bac5f4b790ff8012a3c0b383792cdfc9
sjas <teilzeittresor@web.de>
2014-04-13 15:33:32 +0200

    dunno what that was

--- 13.rb
+++ 13.rb
@@ -4,4 +4,5 @@ puts "The script is called: #{$0}"
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
-#todo combine STDIN.gets.chomp() with ARGV to make a script that gets more input from a user
+
+#TODO combine STDIN.gets.chomp() with ARGV to make a script that gets more input from a user
